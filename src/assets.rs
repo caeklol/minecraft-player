1d23718c6ac4632211273b97420241f37507f27a
caeklol <me@caek.dev>
2025-08-03 14:22:21 +0800

    sound registry & sound definitions

--- src/assets.rs
+++ src/assets.rs
@@ -1,106 +1,260 @@
-use std::{collections::HashMap, fmt::Display, hash::Hash};
+use std::{collections::HashMap, fs::exists, io::Cursor, path::{Path, PathBuf}, sync::{atomic::{AtomicUsize, Ordering}, Arc}};
+
+use anyhow::{anyhow, Error};
 use bytes::Bytes;
+use clap::Parser;
+use futures::stream::{self, FuturesUnordered};
+use lewton::inside_ogg::OggStreamReader;
+use futures::StreamExt;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use serde::{Deserialize, Serialize};
+use tokio::fs;
 
-use anyhow::{Error, anyhow};
-use serde::{Deserialize, Deserializer};
-use serde_json::Value;
-use sha1_smol::Sha1;
+use crate::mojang::{self, AssetIndex, Object, Version};
 
-static VERSION_MANIFEST_URL: &str = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json";
-static ASSET_URL: &str = "https://resources.download.minecraft.net";
-
-#[derive(Deserialize, Clone, Debug)]
-pub struct LatestVersion {
-    pub release: String,
-    pub snapshot: String
+#[derive(Parser, Debug)]
+pub enum FetchBehavior {
+    CacheOnly,
+    Refetch,
+    FetchIfMissing
 }
 
-#[derive(Deserialize, Clone, Debug)]
-pub struct Version {
-    pub id: String,
-    pub url: String
+#[derive(Clone)]
+pub struct Sound {
+    first_tick: Vec<i16>,
+    sample_rate: usize
 }
 
-impl Display for Version {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.id)
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ResourceLocation {
+    pub name: PathBuf,
+    pub volume: Option<f32>,
+    pub pitch: Option<f32>,
+    pub weight: Option<usize>,
+    pub resource_type: Option<String>
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum AudioResourceLocation {
+    Partial(String),
+    Full(ResourceLocation)
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SoundDefinition {
+    pub sounds: Vec<AudioResourceLocation>,
+    pub subtitle: Option<String>
+}
+
+pub async fn fetch_sound_definitions(assets: &PathBuf, version: &Version, behavior: &FetchBehavior, asset_index: &AssetIndex) -> Result<HashMap<String, SoundDefinition>, Error> {
+    let assets_path = assets.join(PathBuf::from(version.id.clone()));
+    let sound_definitions_path = &assets_path.join("sound_definitons.json");
+
+    match behavior {
+        FetchBehavior::CacheOnly => {
+            if fs::try_exists(sound_definitions_path).await? {
+                return Ok(serde_json::from_str(&fs::read_to_string(sound_definitions_path).await?)?)
+            } else {
+                eprintln!("cache-only mode specified without a sound definitions (`sound_definitions.json`) file");
+                println!("help: run with refetch or normal fetch behavior");
+                return Err(anyhow!("missing sound_definitions"))
+            }
+        }
+        FetchBehavior::FetchIfMissing => {
+            if fs::try_exists(sound_definitions_path).await? {
+                return Ok(serde_json::from_str(&fs::read_to_string(sound_definitions_path).await?)?)
+            }
+        },
+        FetchBehavior::Refetch => {}
+    };
+
+    let sound_definition_asset = asset_index.objects.iter().find(|(k, _)| k.ends_with("sounds.json")).expect("could not find `sounds.json` in asset index");
+    let defs_bytes = mojang::fetch_asset(&sound_definition_asset.1.hash).await?;
+    let defs_json = str::from_utf8(&defs_bytes)?;
+    let defs = serde_json::from_str(&defs_json)?;
+    tokio::fs::create_dir_all(assets_path).await.expect("failed to create version directory");
+    tokio::fs::write(sound_definitions_path, defs_json).await.expect("failed to write to file");
+    return Ok(defs);
+}
+
+pub async fn fetch_sounds(cache: &PathBuf, version: &Version, behavior: &FetchBehavior, asset_index: &AssetIndex) -> Result<HashMap<PathBuf, Sound>, Error> {
+    let mut sound_assets_bytes: HashMap<PathBuf, Bytes> = HashMap::new();
+
+    let cache_path = cache.join(PathBuf::from(version.id.clone()));
+    let local_paths: Vec<PathBuf> = visit_dirs(&cache_path)
+        .unwrap_or(vec![])
+        .into_iter()
+        .filter(|path| path.extension().is_some_and(|ext| ext == "ogg"))
+        .collect();
+
+    let remote_objects = match behavior {
+        FetchBehavior::Refetch => {
+            asset_index.objects
+                .iter()
+                .filter(|(key, _)| key.ends_with(".ogg"))
+                .map(|(key, val)| (PathBuf::from(key), val))
+                .collect::<HashMap<PathBuf, &Object>>()
+        },
+        FetchBehavior::FetchIfMissing => {
+            println!("reading local sound assets...");
+            let byte_results = stream::iter(&local_paths)
+                .map(|path| async move {
+                    (path, fs::read(path).await)
+                })
+                .buffer_unordered(512)
+                .collect::<Vec<(&PathBuf, Result<Vec<u8>, std::io::Error>)>>()
+                .await;
+
+            for (sound_path, bytes_res) in byte_results {
+                let sound_path = sound_path.strip_prefix(&cache_path).unwrap();
+                match bytes_res {
+                    Ok(bytes) => {
+                        sound_assets_bytes.insert(sound_path.to_path_buf(), bytes.into());
+                    },
+                    Err(e) => {
+                        eprintln!("failed to read `{:?}`, '{}'", sound_path, e);
+                    },
+                }
+            }
+
+            let mut remote_total = 0;
+            let sound_objects = asset_index.objects
+                .iter()
+                .filter(|(key, _)| {
+                    if key.ends_with(".ogg") {
+                        remote_total += 1;
+                    }
+                    key.ends_with(".ogg")
+                })
+                .map(|(key, val)| (PathBuf::from(key), val))
+                .filter(|(key, _)| !local_paths.contains(&cache_path.join(key)))
+                .collect::<HashMap<PathBuf, &Object>>();
+
+            println!("found remote {} assets and {} local assets. fetching {} assets", remote_total, local_paths.len(), sound_objects.len());
+
+            sound_objects
+        },
+        FetchBehavior::CacheOnly => HashMap::new(),
+    };
+    
+    if !remote_objects.is_empty() {
+        println!("fetching remote assets...");
+
+        let total_requests = Arc::new(AtomicUsize::new(0));
+        let errored_requests = Arc::new(AtomicUsize::new(0));
+
+        let request_results: Vec<(PathBuf, Result<Bytes, Error>)> = stream::iter(remote_objects)
+            .map(|(key, val)| {
+                let total_requests = total_requests.clone();
+                let errored_requests = errored_requests.clone();
+                async move {
+                    let res = (key, mojang::fetch_asset(&val.hash).await);
+
+                    let total = total_requests.load(Ordering::Relaxed);
+                    total_requests.store(total+1, Ordering::Relaxed); 
+                    let errored = errored_requests.load(Ordering::Relaxed);
+                    if res.1.is_err() { 
+                        errored_requests.store(errored+1, Ordering::Relaxed);
+                    }
+
+                    let errored = errored_requests.load(Ordering::Relaxed);
+
+                    print!("total: {}, errored: {}\r", total, errored);
+
+                    res
+                }
+            })
+            .buffer_unordered(512)
+            .collect()
+            .await;
+
+        print!("\n");
+
+        for (sound_path, bytes_res) in request_results {
+            match bytes_res {
+                Ok(bytes) => {
+                    sound_assets_bytes.insert((*sound_path).to_path_buf(), bytes.clone());
+                    let sound_path = cache_path.join(sound_path);
+                    fs::create_dir_all(sound_path.parent().unwrap()).await.expect("failed to create parent sound directory");
+                    fs::write(sound_path, bytes).await.expect("failed to write to file");
+                },
+                Err(e) => {
+                    eprintln!("failed to fetch `{:?}`, '{:?}'", sound_path, e);
+                },
+            }
+        }
     }
+
+    return Ok(sound_assets_bytes
+        .into_par_iter()
+        .map(|(path, bytes)| -> Result<Option<(PathBuf, Sound)>, Error> {
+            let cursor = Cursor::new(bytes);
+
+            let mut ogg_reader = OggStreamReader::new(cursor)
+                .map_err(|e| anyhow!("failed to decode {}, {}", path.to_string_lossy(), e))?;
+
+            let sample_rate: usize = ogg_reader.ident_hdr.audio_sample_rate.try_into().unwrap();
+            let samples_per_tick = (sample_rate * 50) / 1000; 
+
+            let mut samples = Vec::new();
+
+            let stereo = ogg_reader.ident_hdr.audio_channels == 2;
+            
+            while let Some(channels) = ogg_reader.read_dec_packet()
+                .map_err(|e| anyhow!("failed to read packet for {}, {}", path.to_string_lossy(), e))? {
+                if samples.len() >= samples_per_tick {
+                    break
+                }
+
+                if stereo {
+                    let left_channel = &channels[0];
+                    let right_channel = &channels[1];
+
+                    let mut averaged: Vec<i16> = Vec::new();
+                    for index in 0..left_channel.len() {
+                        let avg = (left_channel[index] as i32 + right_channel[index] as i32) / 2i32;
+                        averaged.push(avg.try_into().unwrap());
+                    }
+
+                    samples.extend(averaged);
+                } else {
+                    samples.extend(channels[0].clone());
+                }
+            }
+
+            if samples.len() < samples_per_tick {
+                return Ok(None);
+            }
+
+            let samples = &samples[0..samples_per_tick];
+
+            return Ok(Some((path.to_path_buf(), Sound {
+                first_tick: samples.to_vec(),
+                sample_rate
+            })));
+        })
+        .collect::<Result<Vec<Option<(PathBuf, Sound)>>, Error>>()?
+        .iter()
+        .filter(|t| t.is_some())
+        .map(|t| t.clone().unwrap())
+        .collect::<HashMap<PathBuf, Sound>>()
+    );
 }
+fn visit_dirs(dir: &Path) -> std::io::Result<Vec<PathBuf>> {
+    let mut files = Vec::new();
 
-#[derive(Deserialize, Clone, Debug)]
-pub struct VersionManifest {
-    pub latest: LatestVersion,
-    pub versions: Vec<Version>
-}
-
-pub async fn fetch_version_manifest() -> Result<VersionManifest, Error> {
-    Ok(reqwest::get(VERSION_MANIFEST_URL)
-        .await?
-        .json::<VersionManifest>()
-        .await?
-    )
-}
-
-#[derive(Debug)]
-struct VersionPackage {
-    asset_index_url: String,
-}
-
-impl<'de> Deserialize<'de> for VersionPackage {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let value = Value::deserialize(deserializer)?;
-
-        let url = value
-            .get("assetIndex")
-            .and_then(|ai| ai.get("url"))
-            .and_then(|url| url.as_str())
-            .ok_or_else(|| serde::de::Error::custom("Missing 'assetIndex.url' field"))?
-            .to_string();
-
-        Ok(VersionPackage { asset_index_url: url })
+    if dir.is_dir() {
+        for entry in std::fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                files.extend(visit_dirs(&path)?);
+            } else {
+                files.push(path);
+            }
+        }
     }
+    
+    Ok(files)
 }
-
-#[derive(Deserialize, Clone, Debug)]
-pub struct Object {
-    pub hash: String,
-    pub size: usize
-}
-
-#[derive(Deserialize, Clone, Debug)]
-pub struct AssetIndex {
-    pub objects: HashMap<String, Object>
-}
-
-pub async fn fetch_asset_index(version: &Version) -> Result<AssetIndex, Error> {
-    let package = reqwest::get(&version.url)
-        .await?
-        .json::<VersionPackage>()
-        .await?;
-
-    Ok(reqwest::get(&package.asset_index_url)
-        .await?
-        .json::<AssetIndex>()
-        .await?
-    )
-}
-
-pub async fn fetch_asset(hash: &str) -> Result<Bytes, Error> {
-    let mut hasher = Sha1::new();
-    let response_bytes = reqwest::get(format!("{}/{}/{}", ASSET_URL, &hash[0..2], hash))
-        .await?
-        .bytes()
-        .await?;
-
-    hasher.update(&response_bytes);
-
-    if hasher.digest().to_string() != hash {
-        return Err(anyhow!("repsonse hash did not match asset hash"));
-    }
-
-    return Ok(response_bytes);
-}
-
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
-use std::{collections::HashMap, fs, io, path::{Path, PathBuf}, sync::{atomic::{AtomicUsize, Ordering}, Arc}};
+use std::{collections::HashMap, path::PathBuf};
 
-use futures::{stream, StreamExt};
-use bytes::Bytes;
+use anyhow::{Error, anyhow};
 use clap::Parser;
-use futures::stream::FuturesUnordered;
 use inquire::Select;
-use minecraft_player::assets::{self, Object, Version};
-use anyhow::{anyhow, Error};
-
-#[derive(Parser, Debug)]
-enum FetchBehavior {
-    CacheOnly,
-    Refetch,
-    FetchIfMissing
-}
+use minecraft_player::{assets::{self, AudioResourceLocation, FetchBehavior}, mojang::{self, AssetIndex, Version}};
 
 #[derive(clap::Args, Debug)]
 #[group(required = false, multiple = false)]
@@ -34,189 +24,109 @@ struct Args {
     #[clap(flatten)]
     behavior: BehaviorGroup,
 
-    #[arg(short, long, help = "cache directory (default: ./data)", default_value = "./data")]
-    cache: PathBuf,
+    #[arg(short, long, help = "assets directory (default: ./data)", default_value = "./data")]
+    assets: PathBuf,
 
     #[arg(short, long, help = "input audio file")]
     input: PathBuf,
 }
 
-fn visit_dirs(dir: &Path) -> io::Result<Vec<PathBuf>> {
-    let mut files = Vec::new();
-
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                files.extend(visit_dirs(&path)?);
-            } else {
-                files.push(path);
-            }
-        }
-    }
-    
-    Ok(files)
-}
 
 async fn find_version(target_version: &Option<String>) -> Result<Version, Error> {
     println!("fetching version manifest...");
-    let manifest = assets::fetch_version_manifest().await?;
+    let manifest = mojang::fetch_version_manifest().await?;
     println!("fetched version manifest");
 
-    return Ok(match target_version {
+    match target_version {
         Some(version_str) => {
             let possible_versions = manifest.versions.iter().filter(|v| v.id.contains(version_str)).collect::<Vec<&Version>>();
             let exact_version = manifest.versions.iter().find(|v| v.id == *version_str);
 
+            if let Some(exact_version) = exact_version {
+                return Ok(exact_version.clone())
+            }
+
             if possible_versions.is_empty() {
                 println!("could not find a matching version to `{}`", version_str);
-                Select::new("what version will you use?", manifest.versions).prompt().unwrap()
-            } else if possible_versions.len() == 1 {
-                possible_versions[0].clone()
-            } else if exact_version.is_some() {
-                exact_version.unwrap().clone()
-            } else {
+            } else if possible_versions.len() > 1 {
                 println!("multiple matching versions to `{}`", version_str);
-                Select::new("what version will you use?", possible_versions).prompt().unwrap().clone()
+                return Ok(Select::new("what version will you use?", possible_versions).prompt().unwrap().clone())
+            } else {
+                return Ok(possible_versions[0].clone())
             }
-       },
-        None => Select::new("what version will you use?", manifest.versions).prompt().unwrap(),
-    });
+        },
+        None => {},
+    };
 
+    return Ok(Select::new("what version will you use?", manifest.versions).prompt().unwrap())
 }
 
-async fn load_and_update_sounds(args: &Args) -> Result<HashMap<PathBuf, Bytes>, Error> {
-        let behavior = match (args.behavior.refetch, args.behavior.local) {
-            (true, false) => FetchBehavior::Refetch,
-            (false, true) => FetchBehavior::CacheOnly,
-            (false, false) => FetchBehavior::FetchIfMissing,
-            _ => unimplemented!("impossible")
-        };
-
-    let version = find_version(&args.target_version).await?;
+async fn fetch_sound_registry(
+    version: &Option<String>,
+    assets: &PathBuf,
+    behavior: &FetchBehavior
+) -> Result<HashMap<String, String>, Error> {
+    let version = find_version(version).await?;
     
     let asset_index = match behavior {
         FetchBehavior::FetchIfMissing | FetchBehavior::Refetch => {
             println!("fetching asset index...");
-            assets::fetch_asset_index(&version).await?.objects
+            mojang::fetch_asset_index(&version).await?
         },
-        FetchBehavior::CacheOnly => HashMap::new(),
+        FetchBehavior::CacheOnly => AssetIndex {
+            objects: HashMap::new()
+        },
     };
 
-    let mut sound_assets: HashMap<PathBuf, Bytes> = HashMap::new();
+    println!("fetching sound definitions...");
+    let definitions = assets::fetch_sound_definitions(&assets, &version, &behavior, &asset_index).await?;
 
-    let cache_path = args.cache.join(PathBuf::from(version.id.clone()));
-    let local_paths = visit_dirs(&cache_path).unwrap_or(vec![]);
+    println!("fetching sounds...");
+    let sounds = assets::fetch_sounds(&assets, &version, &behavior, &asset_index).await?;
 
-    let remote_objects = match behavior {
-        FetchBehavior::Refetch => {
-            asset_index
-                .iter()
-                .filter(|(key, _)| key.ends_with(".ogg"))
-                .map(|(key, val)| (PathBuf::from(key), val))
-                .collect::<HashMap<PathBuf, &Object>>()
-        },
-        FetchBehavior::FetchIfMissing => {
-            println!("reading local sound assets...");
-            let futures = local_paths
-                .iter()
-                .map(|path| async move {
-                    (path, tokio::fs::read(path).await)
-                })
-                .collect::<FuturesUnordered<_>>();
+    let mut result = HashMap::new();
 
-            let byte_results = futures.collect::<Vec<(&PathBuf, Result<Vec<u8>, std::io::Error>)>>().await;
-            for (sound_path, bytes_res) in byte_results {
-                let sound_path = sound_path.strip_prefix(&cache_path);
-                match bytes_res {
-                    Ok(bytes) => {
-                        sound_assets.insert(sound_path.expect("failed to strip cache_path off local").to_path_buf(), bytes.into());
-                    },
-                    Err(e) => {
-                        eprintln!("failed to read `{:?}`, '{}'", sound_path, e);
-                    },
-                }
-            }
+    let sound_path = PathBuf::from("minecraft/sounds");
 
-            let mut remote_total = 0;
-            let sound_objects = asset_index
-                .iter()
-                .filter(|(key, _)| {
-                    if key.ends_with(".ogg") {
-                        remote_total += 1;
+    for (identifier, def) in definitions {
+        if def.sounds.len() == 1 {
+            let sound_name = match &def.sounds[0] {
+                AudioResourceLocation::Partial(s) => Some(PathBuf::from(s)),
+                AudioResourceLocation::Full(resource_location) => {
+                    if let Some(resource_type) = &resource_location.resource_type {
+                        if resource_type == "sound" {
+                            Some(resource_location.name.clone())
+                        } else {
+                            None
+                        }
+                    } else {
+                        Some(resource_location.name.clone())
                     }
-                    key.ends_with(".ogg")
-                })
-                .map(|(key, val)| (PathBuf::from(key), val))
-                .filter(|(key, _)| !local_paths.contains(&cache_path.join(key)))
-                .collect::<HashMap<PathBuf, &Object>>();
-
-            println!("found remote {} assets and {} local assets. fetching {} assets", remote_total, local_paths.len(), sound_objects.len());
-
-            sound_objects
-        },
-        FetchBehavior::CacheOnly => HashMap::new(),
-    };
-    
-    if !remote_objects.is_empty() {
-        println!("fetching remote assets...");
-
-        let total_requests = Arc::new(AtomicUsize::new(0));
-        let errored_requests = Arc::new(AtomicUsize::new(0));
-
-        let request_results: Vec<(PathBuf, Result<Bytes, Error>)> = stream::iter(remote_objects)
-            .map(|(key, val)| {
-            let total_requests = total_requests.clone();
-            let errored_requests = errored_requests.clone();
-            async move {
-                let res = (key, assets::fetch_asset(&val.hash).await);
-
-                let total = total_requests.load(Ordering::Relaxed);
-                total_requests.store(total+1, Ordering::Relaxed); 
-                let errored = errored_requests.load(Ordering::Relaxed);
-                if res.1.is_err() { 
-                    errored_requests.store(errored+1, Ordering::Relaxed);
-                }
-
-                let errored = errored_requests.load(Ordering::Relaxed);
-
-
-                print!("total: {} | err: {}\r", total+1, errored);
-
-                res
-            }
-            })
-            .buffer_unordered(512)
-            .collect()
-            .await;
-
-        print!("\n");
-
-        for (sound_path, bytes_res) in request_results {
-            match bytes_res {
-                Ok(bytes) => {
-                    sound_assets.insert((*sound_path).to_path_buf(), bytes.clone());
-                    let sound_path = cache_path.join(sound_path);
-                    tokio::fs::create_dir_all(sound_path.parent().unwrap()).await.expect("failed to create sound directory");
-                    tokio::fs::write(sound_path, bytes).await.expect("failed to write to file");
                 },
-                Err(e) => {
-                    eprintln!("failed to fetch `{:?}`, '{:?}'", sound_path, e);
-                },
+            };
+
+            if let Some(sound_name) = sound_name {
+                let sound = sounds.iter().find(|(path, _)| *path == &sound_path.join(&sound_name));
+                result.insert(identifier, sound);
             }
         }
     }
 
-    return Ok(sound_assets);
+    Err(anyhow!(result.len()))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args = Args::parse();
 
-    let sounds = load_and_update_sounds(&args).await?;
-    println!("sound assets are up to date!");
+    let behavior = match (args.behavior.refetch, args.behavior.local) {
+        (true, false) => FetchBehavior::Refetch,
+        (false, true) => FetchBehavior::CacheOnly,
+        (false, false) => FetchBehavior::FetchIfMissing,
+        _ => unimplemented!("impossible")
+    };
+
+    let sounds = fetch_sound_registry(&args.target_version, &args.assets, &behavior).await?;
 
     println!("reading target file");
     let mut reader = hound::WavReader::open(&args.input)?;
@@ -234,15 +144,15 @@ async fn main() -> Result<(), Error> {
     let sample_rate = reader.spec().sample_rate;
 
     // 20 minecraft ticks per second, (1s/20t) = 0.05s/t = 50ms/t
-    // chunk_size = sample_rate (samples/sec) * duration (ms) / 1000 (ms/sec)
-    //            = sample_rate * 50ms / 1000ms/sec
-    //            = (sample_rate * 50) / 1000 samples
-    let chunk_size = (sample_rate * 50) / 1000; 
-    println!("sample rate of {}Hz, splitting input into {} sized chunks", sample_rate, chunk_size);
+    // samples_per_tick = sample_rate (samples/sec) * duration (ms) / 1000 (ms/sec)
+    //                  = sample_rate * 50ms / 1000ms/sec
+    //                  = (sample_rate * 50) / 1000 samples
+    let samples_per_tick = (sample_rate * 50) / 1000; 
+    println!("sample rate of {}Hz, splitting input into {} sized chunks", sample_rate, samples_per_tick);
 
     // your computer DESERVES to panic if `u32` > `usize` and you are running this. that thing should have left a long time ago. picked
     // up its little feet and ran
-    let chunks = samples.chunks_exact(chunk_size.try_into().unwrap()).collect::<Vec<&[i16]>>();
+    let chunks = samples.chunks_exact(samples_per_tick.try_into().unwrap()).collect::<Vec<&[i16]>>();
     
     for chunk in chunks {
 
